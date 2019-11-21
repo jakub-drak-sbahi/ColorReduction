ea0e5fcefea767c7b23467bd8eef50419e265d24
Jakub Drak Sbahi <jakubdrak98@gmail.com>
2019-11-21 16:46:47 +0100

    Add image loading.

---
+++ gk3/BitmapConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace gk3
+{
+    static class BitmapConverter
+    {
+        public static BitmapImage ConvertBitmapToSource(System.Drawing.Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+    }
+}
--- gk3/MainWindow.xaml.cs
+++ gk3/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace gk3
 {
@@ -20,9 +22,26 @@ namespace gk3
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Bitmap image;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            fileDialog.Title = "Load Texture";
+            fileDialog.Filter = "JPEG Files (*.jpeg)|*.jpeg|PNG Files (*.png)|*.png|JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif";
+            fileDialog.FilterIndex = 4;
+            fileDialog.RestoreDirectory = true;
+            if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                image = new Bitmap(fileDialog.FileName);
+                OriginalImage.Width = image.Width;
+                OriginalImage.Height = image.Height;
+                OriginalImage.Source = BitmapConverter.ConvertBitmapToSource(image);
+            }
         }
     }
 }
